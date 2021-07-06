ccb760e24f6aac77ed2b70b3424f3fb218ff84b8
Prakhar-08 <prakhar.1923it1011@kiet.edu>
2021-07-07 00:11:18 +0530

    Calorie card

---
+++ tha17/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import CalorieCard from './CalorieCard';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="heading-text">Calorie Card</h1>
+      <div className="Container">
+        <CalorieCard title="Burger" source="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNPJSJd1xc824MLjIz0KFVv7JnQScT0Q1PYg&usqp=CAU" text="You have consumed 60 Calories today!" />
+        <CalorieCard title="Pizza" source="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnIS1HNDJ7zuulRfjjL609EEX6eJQnSSRFmQ&usqp=CAU" text="You have consumed 87 Calories today!" />
+        <CalorieCard title="Fries" source="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWOSvNZLSYk0-Al2h8Rebsy3JxKYq1WFMfVw&usqp=CAU" text="You have consumed 58 Calories today!" />
+        <CalorieCard title="Samosa" source="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQP9He6DsZGJBbZRdVySa0G4Nkgsu5owv21Bw&usqp=CAU" text="You have consumed 74 Calories today!" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
