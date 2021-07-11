04d740df871d50157381e6b9073321e98ad642e4
Prakhar-08 <prakhar.1923it1011@kiet.edu>
2021-07-12 02:07:51 +0530

    Hooks Increasing values

---
+++ tha19/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Element from './Element';
+
+function App() {
+  return (
+    <div className="App">
+    <h1 style={{color: "#fff", fontSize: "2.5rem"}}>Click the Buttons to increase their values!</h1>
+      <Element />
+      <Element />
+      <Element />
+      <Element />
+    </div>
+  );
+}
+
+export default App;
