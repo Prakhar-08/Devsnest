5f33530b1df3cab046bb911172c5068a992ff7df
Prakhar-08 <prakhar.1923it1011@kiet.edu>
2021-07-13 22:20:30 +0530

    Loops and Inline styling in React chess Board

---
+++ tha18/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Chess from "./Chess";
+
+
+function App() {
+    return (
+        <div classname="App">
+            <h1 style={{textAlign: "center", marginTop: "100px"}}>Chess Board</h1>
+            <Chess />
+        </div>
+    )
+}
+
+export default App;
